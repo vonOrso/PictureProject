fee840185bb8377bf71560b6b45d60124af5d8cc
vonOrso <43719238+vonOrso@users.noreply.github.com>
2019-07-25 20:39:34 +0300

    v2

--- openpict.py
+++ openpict.py
@@ -5,7 +5,7 @@ def openpic(obj1,obj2,obj3):
     num = T.title(obj1,obj2,obj3)
 
     for i in num:
-        img = Image.open(''.join(r'C:\Users\Вадим\Downloads\kadid10k\kadid10k\images\\' +i ))
+        img = Image.open(''.join(r'C:\Users\Вадим\Downloads\kadid10k\kadid10k\images\\' +i )) #Путь к картинкам
         img.show()
 
 
